1891a4b1eac0ef59d0aab5980ae5422ab9cfbdb0
Herve Donner <hervedonner@gmail.com>
2026-03-23 15:34:54 +0100

    Revert "Reproduce issue (caused by size attribute on form elements?)."
    This reverts commit faed6183700af791b12eb484797727d5ce7939d1.

--- tests/Custom/LargePageClickTest.php
+++ tests/Custom/LargePageClickTest.php
@@ -8,48 +8,14 @@ class LargePageClickTest extends TestCase
 {
     public function testLargePageClick(): void
     {
-        $this->getSession()->visit($this->pathTo('/advanced_form.html'));
-        // @todo Why is size attribute causing ElementClickIntercepted errors?
-        $this->getSession()->executeScript('document.querySelector("input[name=\'first_name\']").setAttribute("size", 200);');
+        $this->getSession()->visit($this->pathTo('/multi_input_form.html'));
 
-        // Add a large amount of br tags so that form elements are not in view.
+        // Add a large amount of br tags so that the button is not in view.
         $this->makePageLong();
 
         $page = $this->getSession()->getPage();
-
-        // Test select focus.
-        $this->scrollToTop();
-        $page->selectFieldOption('select_number', 'thirty');
-
-        // Test radio button focus.
-        $this->scrollToTop();
-        $page->selectFieldOption('sex', 'm');
-
-        // Test checkboxes focus.
-        $this->scrollToTop();
-        $page->uncheckField('mail_list');
-        $this->scrollToTop();
-        $page->checkField('agreement');
-
-        // Test button focus and submit.
-        $this->scrollToTop();
         $page->pressButton('Register');
-
-        $expected = <<<EOF
-array(
-  agreement = `on`,
-  email = `your@email.com`,
-  first_name = `Firstname`,
-  last_name = `Lastname`,
-  notes = `original notes`,
-  select_number = `30`,
-  sex = `m`,
-  submit = `Register`,
-)
-no file
-EOF;
-
-        $this->assertStringContainsString($expected, $page->getContent());
+        $this->assertStringContainsString('no file', $page->getContent());
     }
 
     public function testDragDrop(): void
@@ -79,13 +45,6 @@ EOF;
             document.body.insertBefore(p, document.body.firstChild);
         JS;
         $this->getSession()->executeScript($script);
-    }
-
-    /**
-     * Scrolls to the top of the page.
-     */
-    private function scrollToTop(): void {
-        $this->getSession()->executeScript('window.scrollTo(0, 0);');
     }
 
 }
